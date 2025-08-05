600975753353151f4f27932a65c9346801717eab
TraderMigs <wvdreamsofimd@gmail.com>
2025-08-06 00:34:21 +0700

    Create create-checkout-session.js

---
+++ functions/create-checkout-session.js
@@ -0,0 +1,15 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const session = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    line_items: [{ price: "price_1RsjJZRrIlnVe6VQwjaCfrXo", quantity: 1 }],
+    success_url: `chrome-extension://${process.env.EXT_ID}/thankyou.html`,
+    cancel_url: `chrome-extension://${process.env.EXT_ID}/popup.html`
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ url: session.url })
+  };
+};
